d0a5761804dc021300ed712833c92be0f6dbca4c
Lo0425 <chingclo.lcc@gmail.com>
2022-12-20 13:41:18 +0800

    second commit

--- app/src/main/java/com/xiangze/filemanager/MainFragment.kt
+++ app/src/main/java/com/xiangze/filemanager/MainFragment.kt
@@ -1,18 +1,54 @@
 package com.xiangze.filemanager
 
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.xiangze.filemanager.adapters.FileAdapter
+import com.xiangze.filemanager.databinding.FragmentMainBinding
+import java.io.File
 
 class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+    private lateinit var adapter: FileAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val args: MainFragmentArgs by navArgs()
+        val path = if(args.path != null && args.path != "null"){
+            args.path!!
+        }else{
+            Environment.getExternalStorageDirectory().path
+        }
+
+        val root = File(path)
+        root.listFiles()?.let {
+            setupAdapter(it.toList())
+        }
+    }
+
+    private fun setupAdapter(files: List<File>){
+        val layoutManager = LinearLayoutManager(requireContext())
+        adapter = FileAdapter(files){
+            val action = MainFragmentDirections.actionMainToSelf(it.path)
+            NavHostFragment.findNavController(this).navigate(action)
+        }
+        binding.rvFiles.layoutManager = layoutManager
+        binding.rvFiles.adapter = adapter
     }
 }
--- app/src/main/java/com/xiangze/filemanager/MyApplication.kt
+++ app/src/main/java/com/xiangze/filemanager/MyApplication.kt
@@ -1,7 +1,8 @@
 package com.xiangze.filemanager
 
+import android.app.Application
 import com.xiangze.filemanager.repository.TaskRepository
 
-class MyApplication {
+class MyApplication: Application() {
     val taskRepo = TaskRepository.getInstance()
 }
