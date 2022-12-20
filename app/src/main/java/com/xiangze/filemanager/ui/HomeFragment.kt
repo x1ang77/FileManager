c79efc58ba677824d8b008f1f215b58176ce987e
nathalie7980 <londonhippos@gmail.com>
2022-12-20 10:41:20 +0800

    add fragment container view

--- app/src/main/java/com/xiangze/filemanager/ui/HomeFragment.kt
+++ app/src/main/java/com/xiangze/filemanager/ui/HomeFragment.kt
@@ -1,19 +1,30 @@
 package com.xiangze.filemanager.ui
 
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.xiangze.filemanager.R
+import com.xiangze.filemanager.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val path = Environment.getExternalStorageDirectory()
+        Log.d("debugging", path.toString())
+    }
 }
