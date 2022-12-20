58e54e7e6243170d23ae92de82103ab643d4e95a
nathalie7980 <londonhippos@gmail.com>
2022-12-20 13:40:40 +0800

    third commit

--- app/src/main/java/com/xiangze/filemanager/ui/HomeFragment.kt
+++ app/src/main/java/com/xiangze/filemanager/ui/HomeFragment.kt
@@ -7,11 +7,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.xiangze.filemanager.R
+import com.xiangze.filemanager.adapters.FileAdapter
 import com.xiangze.filemanager.databinding.FragmentHomeBinding
+import java.io.File
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var adapter: FileAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,10 +28,30 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val path = Environment.getExternalStorageDirectory()
-        Log.d("debugging", path.toString())
+        val navArgs: HomeFragmentArgs by navArgs()
+
+        val path = if (navArgs.path != null && navArgs.path != "null") {
+            navArgs.path!!
+        } else {
+            Environment.getExternalStorageDirectory().path
+        }
+
+        val root = File(path)
+        root.listFiles()?.let {
+            setupAdapter(it.toList())
+        }
+    }
+
+    private fun setupAdapter(files: List<File>) {
+        val layoutManager = LinearLayoutManager(requireContext())
+        adapter = FileAdapter(files) {
+            val action = HomeFragmentDirections.actionHomeToSelf(it.path)
+            NavHostFragment.findNavController(this).navigate(action)
+        }
+        binding.rvFiles.layoutManager = layoutManager
+        binding.rvFiles.adapter = adapter
+
     }
 }
