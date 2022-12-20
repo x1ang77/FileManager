b041154a353438666d38b2e2d10b0ea863789277
Joel Seah <quanshenjoelmichael21s@forwardschool.edu.my>
2022-12-20 10:41:20 +0800

    Second commit

--- app/src/main/java/com/xiangze/filemanager/fragments/HomeFragment.kt
+++ app/src/main/java/com/xiangze/filemanager/fragments/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.xiangze.filemanager.fragments
 
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -16,5 +18,11 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val path = Environment.getExternalStorageDirectory()
+        Log.d("path", "$path")
     }
 }
