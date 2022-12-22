3e14345edcf61970149f576b7bc9a52ff1b5bb1c
GooEySnow <yanhanphua2@gmail.com>
2022-12-22 10:34:02 +0800

    im done

--- app/src/main/java/com/xiangze/filemanager/ui/ImageViewerFragment.kt
+++ app/src/main/java/com/xiangze/filemanager/ui/ImageViewerFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.RecyclerView
+import com.xiangze.filemanager.MainActivity
 import com.xiangze.filemanager.adapters.ImageSliderAdapter
 import com.xiangze.filemanager.databinding.FragmentImageViewerBinding
 
